7450548cb98fe4e13aa139d638729756cc9a8fd0
Crystal <Crystal@DESKTOP-81QTH9S>
2021-09-30 21:16:29 -0400

    adding is fun

--- src/demo/DemoTwo.java
+++ src/demo/DemoTwo.java
@@ -4,6 +4,13 @@ public class DemoTwo {
 	
 	public static void main(String[] args) {
 		System.out.println("My second demo");
+		
+		
+		
+		int a = 10;
+		int b = 20;
+		
+		System.out.println(a+b);
 	}
 
 }
